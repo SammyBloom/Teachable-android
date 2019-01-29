92dad9857e296f2e45f140e1ebfcf1edd0580e54
SammyBloom <nwachiemi@gmail.com>
2019-01-29 15:48:54 +0100

    Added login and registeration screens for Individuals

---
+++ app/src/main/java/com/example/frebby/teachable/Individual_RegistrationOption.java
@@ -0,0 +1,13 @@
+package com.example.frebby.teachable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Individual_RegistrationOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual__registration_option);
+    }
+}
